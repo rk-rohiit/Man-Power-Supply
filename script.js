9d677ace9a377915d76bd4acd5c9f9a12230d15e
rk-rohiit <askrohiitsharma@gmail.com>
2023-10-10 11:07:02 +0530

    popup and responsive upgrade

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Open Join Now Button
+
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
   }
   function closeForm() {
     document.getElementById("popupForm").style.display = "none";
   }
+
+  // Block Inspect Mode
+
+  document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+    alert('Inspect mode is disabled.');
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12') {
+        e.preventDefault();
+        alert('Developer tools are disabled.');
+    }
+});
