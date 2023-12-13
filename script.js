7f40a750d21de6beb73f2c63c5975673f8891147
rk-rohiit <askrohiitsharma@gmail.com>
2023-12-13 17:21:02 +0530

    pop-up and responsive uograded

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-
 //Loader
 window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader-wrapper");
-    loader.style.display = "none"; // Hide the loader
+  const loader = document.querySelector(".loader-wrapper");
+  loader.style.display = "none"; // Hide the loader
 });
 
 // Open Join Now Button
 
 function openForm() {
-    document.getElementById("popupForm").style.display = "block";
-  }
-  function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
-  }
+  document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
 
+// Block Inspect Mode
 
-  
-  // Block Inspect Mode
-
-  document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-    alert('Inspect mode is disabled.');
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  alert("Inspect mode is disabled.   (Rohit Sharma)");
 });
 
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'F12') {
-        e.preventDefault();
-        alert('Developer tools are disabled.');
-    }
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12") {
+    e.preventDefault();
+    alert("Developer tools are disabled.");
+  }
 });
